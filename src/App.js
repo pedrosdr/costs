5c4ab6db8a390c144949672c74c0ba17f69873d4
Pedro Sartori <pedrosartdr@gmail.com>
2023-08-22 01:29:57 -0300

    navbar e footer

--- src/App.js
+++ src/App.js
@@ -4,33 +4,24 @@ import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 import Container from "./components/layout/Container"
+import Navbar from "./components/layout/Navbar"
+import Projects from "./components/pages/Projects"
+import Footer from "./components/layout/Footer"
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/company">Empresa</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contato</Link>
-        </li>
-        <li>
-          <Link to="/newproject">Novo projeto</Link>
-        </li>
-      </ul>
+      <Navbar/>
       <Container customClass='min-height'>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/company" element={<Company/>}/>
           <Route exact path="/contact" element={<Contact/>}/>
           <Route exact path="/newproject" element={<NewProject/>}/>
+          <Route exact path="/projects" element={<Projects/>}/>
         </Routes>
       </Container>
-      <footer>Footer</footer>
+      <Footer/>
     </Router>
   )
 }
